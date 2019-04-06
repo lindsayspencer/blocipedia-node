7b86625fa336e3b910683e60be1efcfd52e737a1
Lindsay Spencer <43455549+lindsayspencer@users.noreply.github.com>
2019-04-06 14:05:03 -0500

    scaffolding checkpoint complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,9 @@
+const logger = require('morgan');
+
 module.exports = {
     init(app){
         const staticRoutes = require('../routes/static');
         app.use(staticRoutes);
+        app.use(logger('dev'));
     }
 }
